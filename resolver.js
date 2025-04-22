1f12c556dc64fd051476e45ec5ae570a628f9916
Karan Maheshwari <krn.maheshwari9@gmail.com>
2025-04-22 21:54:41 +0530

    lesson 7 is completed - added new reolvers for related data querying and resolve the problem of LF- CRLF

--- resolver.js
+++ resolver.js
@@ -20,5 +20,23 @@ export const resolvers = {
         review(_, args) {
             return db.reviews.find((review) => review.id === args.id)
         }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.author_id === parent.id)
+        }
+    },
+    Review: {
+        game(parent) {
+            return db.games.find((game) => game.id === parent.game_id)
+        },
+        author(parent) {
+            return db.authors.find((author) => author.id === parent.author_id)
+        }
     }
 }
